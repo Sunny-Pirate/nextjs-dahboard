e4a846372695eef8feaa00a0777a6d82992d9d89
Luca Faccio <luca@sunnyday.software>
2023-11-11 13:31:58 +0100

    Chapter 16 - Adding metadata

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,9 @@
 import type {NextAuthConfig} from 'next-auth';
 
+
+const protectedRoutes = ["/dashboard", "/profile"];
+const isRouteProtected = (path: string) => protectedRoutes.some(protectedPath => path.startsWith(protectedPath));
+
 export  const authConfig = {
     providers:[],
     pages: {
@@ -8,12 +12,12 @@ export  const authConfig = {
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
-            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-            if (isOnDashboard) {
+            const currentPath = nextUrl.pathname;
+            if (isRouteProtected(currentPath)) {
                 if (isLoggedIn) return true;
                 return false; // Redirect unauthenticated users to login page
             } else if (isLoggedIn) {
-                return Response.redirect(new URL('/dashboard', nextUrl));
+                return Response.redirect(new URL("/dashboard", nextUrl));
             }
             return true;
         },
